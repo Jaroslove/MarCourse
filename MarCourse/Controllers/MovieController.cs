a4ed5767fc3f75617246cb475e2b8cdb658df9dd
Jaroslove <jroslovemonro@gmail.com>
2018-02-05 20:35:21 +0300

    added boots-lumen

---
+++ MarCourse/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarCourse.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            var movie = new Models.Movie { Name = "Shreck" };
+            return View(movie);
+        }
+    }
+}
